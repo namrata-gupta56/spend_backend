81bb399f307a50d1a6ba30f9f2e7e12c978198c4
namrata-gupta56 <157316105+namrata-gupta56@users.noreply.github.com>
2024-01-22 01:49:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose
     }
   )
   .then(() => console.log("Connected to MongoDB"))
-   .catch((err) => {
+  .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
     process.exit(1); // Exit the process if the connection fails
   });
