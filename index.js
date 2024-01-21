0065b63e134a47598314b853eb9057c40c0af654
namrata-gupta56 <157316105+namrata-gupta56@users.noreply.github.com>
2024-01-22 02:14:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-var cors = require('cors')
+const cors = require('cors')
 const multer = require("multer");
 const app = express();
 app.use(cors())
